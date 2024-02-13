fc3c0227d483e090047fc4d5693e2491dac9becc
YixinWan <117781611+YixinWan@users.noreply.github.com>
2024-02-13 11:39:33 +0200

    Add files via upload

--- new_8T.py
+++ new_8T.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 """
 This experiment was created using PsychoPy3 Experiment Builder (v2023.2.2),
-    on 二月 12, 2024, at 12:54
+    on 二月 12, 2024, at 13:17
 If you publish work using this script the most relevant publication is:
 
     Peirce J, Gray JR, Simpson S, MacAskill M, Höchenberger R, Sogo H, Kastman E, Lindeløv JK. (2019) 
@@ -107,7 +107,7 @@ def setupData(expInfo, dataDir=None):
     thisExp = data.ExperimentHandler(
         name=expName, version='',
         extraInfo=expInfo, runtimeInfo=None,
-        originPath='D:\\PSYCHOPY\\eyehand_baseline\\new_8T_lastrun.py',
+        originPath='D:\\PSYCHOPY\\eyehand_baseline\\new_8T.py',
         savePickle=True, saveWideText=True,
         dataFileName=dataDir + os.sep + filename, sortColumns='time'
     )
